5db87fff59079ca66c0eb714b6dd7a4e1e79a23e
JAYAPRAKASH <ajayaprakash98@gmail.com>
2020-09-07 22:43:26 -0700

    Create Greedy person.py

---
+++ Greedy person.py
@@ -0,0 +1,103 @@
+'''    QUESTION:
+
+Greedy person:
+
+The owner of the shopping complex is a greedy person. The owner is very lazy to do anything and so felt lazy to count the number of digits in profit. 
+Also, he doesn't believe in anyone to assign that job. So he wants to make someone write a program to count the number of digits in the profit. 
+Can you make a program to calculate it?
+
+Input Format : The input contains an integer n
+
+Input Constraints : 1<=|n|<=10^7
+
+Output Format : Print the result
+
+Sample Input :
+2342
+
+Sample Output :
+4
+
+
+
+
+
+
+
+
+
+
+HINTS:
+
+1 - When you divide a number by 10, the number of digits reduce by 1.
+
+2 - Use a counter variable. Run a loop till N>0. In each iteration, divide N by 10 and increment the counter by 1.
+
+3 - In python, you can handle the number as a string as well.
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ANSWER:
+
+x = input()
+print(len(x))
